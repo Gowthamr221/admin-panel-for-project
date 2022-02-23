9794e1d62372b87efa2d6bf99601c52b181c5a76
Gowthamr221 <gowthamr221@gmail.com>
2022-02-23 11:23:33 +0530

    post request implementation

--- project.js
+++ project.js
@@ -1,16 +1,15 @@
 const inputFile = document.getElementById("file");
 
 var eventForum = document.getElementById("events-form");
-eventForum.addEventListener("submit", async (e) => {
+eventForum.addEventListener("submit",async (e) => {
   e.preventDefault();
   var formdata = new FormData();
 
   
   formdata.append("name", e.target.name.value);
-  formdata.append("url", e.target.url.value);
+  
   formdata.append("content", e.target.content.value);
   formdata.append("date", e.target.date.value);
-  formdata.append("time", e.target.time.value);
   formdata.append("file1", inputFile.files[0]);
 
   var requestOptions = {
@@ -18,7 +17,7 @@ eventForum.addEventListener("submit", async (e) => {
     body: formdata,
     redirect: "follow",
   };
-
+  
   await fetch("https://safe-taiga-20513.herokuapp.com/event", requestOptions)
     .then((response) => response.text())
     .then((result) => console.log(result))
